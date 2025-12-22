a473bf493ef74e24c6874783c8031d6c83556f50
ChilikB <bohdan.kandela@gmail.com>
2025-12-22 13:47:47 +0200

    done(): homework 3

--- CProject/CPlusProject/CPlusProject/CPlusProject.cpp
+++ CProject/CPlusProject/CPlusProject/CPlusProject.cpp
@@ -7,6 +7,9 @@
 #include <cstdlib>
 #include "Character.h"
 #include "Utils.h"
+#include <utility>
+#include <map>
+#include <vector>
 
 Character* character = nullptr;
 
@@ -18,13 +21,14 @@ Character* character = nullptr;
 int menu();
 int menu2();
 int menu3(Character* players, int count);
-
+int menu4();
 Character createCharacter();
 
 void main()
 {
     //while (menu());
-    while (menu2());
+    //while (menu2());
+	while (menu4());
 }
 
 void homework2() {
@@ -53,6 +57,124 @@ void homework2() {
 
     delete[] players;
     players = nullptr;
+}
+
+void homework3Task1() {
+    int counter = 0;
+    std::cout << "Counter:" << std::endl;
+    std::cin >> counter;
+    if (std::cin.fail()) {
+        throw std::invalid_argument("Invalid input for counter. Please enter a valid number.");
+    }
+    std::map<int, int> units;
+    int type = 0;
+    int value = 0;
+    for (int i = 0; i < counter; i++) {
+		std::cout << "Unit type (int):" << std::endl;
+		std::cin >> type;
+        if (std::cin.fail()) {
+            throw std::invalid_argument("Invalid input for unit type. Please enter a valid number.");
+        }
+        std::cout << "Units count (int):" << std::endl;
+        std::cin >> value;
+        if (std::cin.fail()) {
+            throw std::invalid_argument("Invalid input for unit count. Please enter a valid number.");
+        }
+
+        if(units.find(type) != units.end()) {
+            units[type] += value;
+        } else {
+            units[type] = value;
+        }
+        system("cls");
+    }
+
+    if (units.empty()) {
+        std::cout << "No units entered." << std::endl;
+        return;
+    }
+    
+    auto value_comparer = [](const auto& p1, const auto& p2) {
+        return p1.second < p2.second;
+    };
+
+    Utils::SetConsoleColor(11, 0);
+    
+    auto minValue = std::min_element(units.begin(), units.end(), value_comparer);
+    std::cout << "Min (unit type): " << minValue->first << ", (unit count): " << minValue->second << std::endl;
+
+    auto maxValue = std::max_element(units.begin(), units.end(), value_comparer);
+    std::cout << "Max (unit type): " << maxValue->first << ", (unit count): " << maxValue->second << std::endl;
+
+    Utils::ResetConsoleColor();
+}
+void homework3Task2() {
+    int counter = 0;
+    std::cout << "Steps counter:" << std::endl;
+    std::cin >> counter;
+    if (std::cin.fail()) {
+        throw std::invalid_argument("Invalid input for counter. Please enter a valid number.");
+    }
+
+    std::vector<std::string> path;
+    std::string direction;
+	//awailable directions
+	std::vector<std::string> directions = { "left", "right", "top", "down" };
+
+    system("cls");
+
+    for (int i = 0; i < counter; i++) {
+        std::cout << "Current path: ";
+        for(int j = 0; j < path.size(); j++) {
+			std::cout << path[j] + " ";
+		}
+        std::cout << std::endl;
+        std::cout << "Enter direction (left,right,top,down):" << std::endl;
+        std::cin >> direction;
+        if (std::find(directions.begin(), directions.end(), direction) != directions.end()) {
+			path.push_back(direction);
+        }
+        else {
+            i--;
+        }
+        system("cls");
+    }
+
+    int x = 0;
+	int y = 0;
+
+    std::cout << "Player coordinate X:" << std::endl;
+    std::cin >> x;
+    if (std::cin.fail()) {
+        throw std::invalid_argument("Invalid input for coordinate X. Please enter a valid number.");
+    }
+    std::cout << "Player coordinate Y:" << std::endl;
+    std::cin >> y;
+    if (std::cin.fail()) {
+        throw std::invalid_argument("Invalid input for coordinate Y. Please enter a valid number.");
+    }
+
+    std::pair<int, int> coords(x, y);
+
+    for (const auto& dir : path) {
+        if (dir == "left") {
+            coords.first--;
+        } else if (dir == "right") {
+            coords.first++;
+        } else if (dir == "top") {
+            coords.second++;
+        } else if (dir == "down") {
+            coords.second--;
+        }
+    }
+    
+    system("cls");
+
+    Utils::SetConsoleColor(11, 0);
+
+    std::cout << "Final coordinates: (" << coords.first << ", " << coords.second << ")" << std::endl;
+
+    Utils::ResetConsoleColor();
 }
 
 int menu() {
@@ -233,6 +355,62 @@ int menu3(Character* players, int count) {
     }
     break;
     case 4:
+        delete character;
+        character = nullptr;
+        return 0;
+    default:
+        system("cls");
+        Utils::SetConsoleColor(4, 0);
+        std::cout << "Invalid choice. Please select again." << std::endl;
+        Utils::ResetConsoleColor();
+        break;
+    }
+
+    return 1;
+}
+
+int menu4() {
+    std::cout << "1. Task 1" << std::endl;
+	std::cout << "2. Task 2" << std::endl;
+    std::cout << "3. Exit" << std::endl;
+
+    int choice;
+    std::cin >> choice;
+
+    switch (choice) {
+    case 1:
+    {
+        try {
+            system("cls");
+            homework3Task1();
+        }
+        catch (std::invalid_argument& e) {
+            system("cls");
+            Utils::SetConsoleColor(4, 0);
+            std::cout << e.what() << std::endl;
+            Utils::ResetConsoleColor();
+            std::cin.clear();
+            std::cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n');
+        }
+    }
+    break;
+    case 2:
+    {
+        try {
+            system("cls");
+            homework3Task2();
+        }
+        catch (std::invalid_argument& e) {
+            system("cls");
+            Utils::SetConsoleColor(4, 0);
+            std::cout << e.what() << std::endl;
+            Utils::ResetConsoleColor();
+            std::cin.clear();
+            std::cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n');
+        }
+    }
+    break;
+    case 3:
         delete character;
         character = nullptr;
         return 0;
